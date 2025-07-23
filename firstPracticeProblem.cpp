bfcd05ff31782d642e714a496fc9437332150fb0
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-23 14:09:26 +0600

    Solved first practice problem

--- firstPracticeProblem.cpp
+++ firstPracticeProblem.cpp
@@ -13,6 +13,16 @@ public:
         integerVector.push_back(integerValue);
     }
 
+    void pop()
+    {
+        integerVector.pop_back();
+    }
+
+    int top()
+    {
+        return integerVector.back();
+    }
+
     int size()
     {
         return countSize;
@@ -27,6 +37,48 @@ public:
 int main()
 {
     myStack firstStack, secondStack;
+
+    int firstSize;
+    cin >> firstSize;
+
+    for (int i = 0; i < firstSize; i++)
+    {
+        int integerValue;
+        cin >> integerValue;
+        firstStack.push(integerValue);
+    }
+
+    int secondSize;
+    cin >> secondSize;
+
+    for (int i = 0; i < secondSize; i++)
+    {
+        int integerValue;
+        cin >> integerValue;
+        secondStack.push(integerValue);
+    }
+
+    if (firstSize != secondSize)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    while (!firstStack.empty())
+    {
+        if (firstStack.top() != secondStack.top())
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+        else
+        {
+            firstStack.pop();
+            secondStack.pop();
+        }
+    }
+
+    cout << "YES" << endl;
 
     return 0;
 }
